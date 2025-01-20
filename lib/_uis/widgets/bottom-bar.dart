f36b3b3b912a3f45d36ea51b59099bc099f695ea
tung.trinh <tung.trinh@realestatedoc.co>
2025-01-20 18:00:21 +0700

    init router

--- lib/_uis/widgets/bottom-bar.dart
+++ lib/_uis/widgets/bottom-bar.dart
@@ -6,20 +6,20 @@ class BottomBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomAppBar(
-      child: Row(
+      child: Flex(
+        direction: Axis.horizontal,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           IconButton(
             onPressed: null,
             icon: Icon(Icons.home_sharp),
             tooltip: 'Home',
           ),
-          Spacer(),
           IconButton(
             onPressed: null,
             icon: Icon(Icons.touch_app_outlined),
             tooltip: 'Airdrop',
           ),
-          Spacer(),
           IconButton(
             onPressed: null,
             icon: Icon(Icons.multiline_chart_sharp),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tt_flutter_airdrop_service/_uis/widgets/layout.dart';
-import 'package:tt_flutter_airdrop_service/router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,57 +16,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: AppRoutes.HOME,
-      onGenerateRoute: AppRoutes.generateRoute,
+      // initialRoute: AppRoutes.HOME,
+      // onGenerateRoute: AppRoutes.generateRoute,
       home: LayoutWidget(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
