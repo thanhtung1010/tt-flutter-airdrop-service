96e1320b0e0f2450d081ac7d379fe0fe9be55275
tung.trinh <tung.trinh@realestatedoc.co>
2025-02-10 14:51:28 +0700

    update bottom bar

--- lib/_uis/widgets/bottom-bar.dart
+++ lib/_uis/widgets/bottom-bar.dart
@@ -14,56 +14,100 @@ class BottomBarWidgetState extends State<BottomBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      selectedIndex: _selectedIndex,
-      labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-      destinations: <Widget>[
-        NavigationDestination(
-          icon: IconButton(
-            onPressed: () {
-              context.router.push(HomeRoute());
-              setState(() {
-                _selectedIndex = 0;
-              });
-            },
-            icon: Icon(Icons.home_sharp),
-            hoverColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-          ),
-          label: 'Home',
+    return BottomAppBar(
+      shape: CircularNotchedRectangle(),
+      color: Colors.deepPurpleAccent,
+      child: IconTheme(
+        data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+        child: Flex(
+          direction: Axis.horizontal,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            TextButton(
+              onPressed: () {
+                if (!context.router.isRouteActive('HomeRoute')) {
+                  context.router.push(HomeRoute());
+                }
+                setState(() {
+                  _selectedIndex = 0;
+                });
+              },
+              style: TextButton.styleFrom(
+                iconColor: Colors.white,
+                iconSize: 25,
+                textStyle: TextStyle(color: Colors.white),
+              ),
+              child: Flex(
+                direction: Axis.horizontal,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                spacing: 4,
+                children: <Widget>[
+                  Icon(Icons.home_outlined),
+                  _selectedIndex == 0 ? Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ) : Text(''),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                if (!context.router.isRouteActive('AirdropRoute')) {
+                  context.router.push(AirdropRoute());
+                }
+                setState(() {
+                  _selectedIndex = 1;
+                });
+              },
+              style: TextButton.styleFrom(
+                iconColor: Colors.white,
+                iconSize: 25,
+                textStyle: TextStyle(color: Colors.white),
+              ),
+              child: Flex(
+                direction: Axis.horizontal,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.touch_app_outlined),
+                  _selectedIndex == 1 ? Text(
+                    'Tap',
+                    style: TextStyle(color: Colors.white),
+                  ) : Text(''),
+                ],
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                if (!context.router.isRouteActive('DepinRoute')) {
+                  context.router.push(DepinRoute());
+                }
+                setState(() {
+                  _selectedIndex = 2;
+                });
+              },
+              style: TextButton.styleFrom(
+                iconColor: Colors.white,
+                iconSize: 25,
+                textStyle: TextStyle(color: Colors.white),
+              ),
+              child: Flex( 
+                direction: Axis.horizontal,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.multiline_chart_sharp),
+                  _selectedIndex == 2 ? Text(
+                    'Depin',
+                    style: TextStyle(color: Colors.white),
+                  ) : Text(''),
+                ],
+              ),
+            ),
+          ],
         ),
-        NavigationDestination(
-          icon: IconButton(
-            onPressed: () {
-              context.router.push(AirdropRoute());
-              setState(() {
-                _selectedIndex = 1;
-              });
-            },
-            icon: Icon(Icons.touch_app_outlined),
-            hoverColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-          ),
-          label: 'Airdrop',
-        ),
-        NavigationDestination(
-          icon: IconButton(
-            onPressed: () {
-              context.router.push(DepinRoute());
-              setState(() {
-                _selectedIndex = 2;
-              });
-            },
-            icon: Icon(Icons.multiline_chart_sharp),
-            hoverColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            highlightColor: Colors.transparent,
-          ),
-          label: 'Depin',
-        ),
-      ],
+      ),
     );
   }
 }
