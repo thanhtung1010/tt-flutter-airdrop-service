a23d6022b99830bc273d5b55a1946d6af6a2aeea
tung.trinh <tung.trinh@realestatedoc.co>
2025-02-17 17:49:19 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:tt_flutter_airdrop_service/_router/router.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 // import 'package:tt_flutter_airdrop_service/_uis/widgets/bottom-bar.dart';
 // import 'package:tt_flutter_airdrop_service/_uis/widgets/header.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(AirdropService());
 }
 
